8b2618771d1f5bd93dec8da6db5683492697c4c9
Ghislain ETIEN <ghislain.etien@outlook.com>
2023-05-09 22:42:03 +0000

    create container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
@@ -20,10 +19,7 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared: ['react', 'react-dom'],
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
+            shared: packageJson.dependencies,
         })
     ]
 }
